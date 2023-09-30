194d2747d23c4f0ecf3f0548a32782d30a38bf11
root <roundtable@gmail.com>
2023-09-30 07:10:59 +0000

    Update script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 python sdxl_minimal_inference.py \
-    --ckpt_path=models/sd_xl_base_1.0.safetensors \
-    --prompt="a photo of hoge person" \
-    --negative_prompt="((distorted eye)), NSFW, cartoon, 3d, tooth, teeth, (disfigured), (bad art), (deformed), (poorly drawn), (extra limbs), asymmetric eyes, (close up), strange colours, blurry, boring, sketch, lackluster, face portrait, self-portrait, signature, letters, watermark, grayscale, low quality, worst quality, distorted hands, mutated hands, mutated finger, crooked nose, uneven teeth, face tattoo, gloomy, depressed, dots, dirty, asymmetric lips, grains, fused hands, fused heads, fused characters. foreground characters, airbrushed, glossy, photoshop" \
+    --ckpt_path=models/leosam.safetensors \
+    --prompt="leogirl, realistic Documentary photography, hoge girl" \
+    --negative_prompt="(worst quality, low quality, cgi, bad eye, worst eye, illustration, cartoon), deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, open mouth" \
     --output_dir="inference_results" \
-    --lora_weights="output/text-encoder-kr/test.safetensors"
+    --lora_weights="output/text-encoder-kr-leosam-768-128/test.safetensors"
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,22 +1,23 @@
 accelerate launch --num_cpu_threads_per_process 1 sdxl_train_network.py \
-    --pretrained_model_name_or_path=models/sd_xl_base_1.0.safetensors \
+    --pretrained_model_name_or_path=models/leosam.safetensors \
     --dataset_config=_typos.toml \
-    --output_dir=output/text-encoder-kr2 \
+    --output_dir=output/locon-cropped_kr-leosam-512-32-3e-4-face \
     --output_name=test \
     --max_train_steps=1000 \
     --save_model_as=safetensors \
     --prior_loss_weight=1.0 \
-    --learning_rate=4e-4 \
+    --learning_rate=3e-4 \
     --lr_scheduler="cosine" \
     --optimizer_type="AdamW8bit" \
     --xformers \
     --mixed_precision="fp16" \
     --cache_latents \
-    --text_encoder_lr=4e-4 \
+    --text_encoder_lr=3e-4 \
     --gradient_checkpointing \
-    --save_every_n_epochs=20 \
-    --sample_every_n_epochs=20 \
+    --save_every_n_epochs=50 \
+    --sample_every_n_epochs=10 \
     --sample_prompts="sample_prompt.json"\
     --no_half_vae \
-    --network_dim=128 \
-    --network_module=networks.lora
+    --network_dim=32 \
+    --network_args "conv_dim=32" "conv_alpha=1.0" "algo=locon"\
+    --network_module=lycoris.kohya
