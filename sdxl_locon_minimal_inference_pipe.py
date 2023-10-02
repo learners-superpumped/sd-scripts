1e09feea782e7523fdde2eb6b3f5517f939e9b29
root <roundtable@gmail.com>
2023-10-02 15:32:59 +0000

    Update script

--- sdxl_locon_minimal_inference_pipe.py
+++ sdxl_locon_minimal_inference_pipe.py
@@ -29,28 +29,38 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=1)
     parser.add_argument("--num_inference_steps", type=int, default=40)
-    parser.add_argument("--num_images_per_prompt", type=int, default=4)
+    parser.add_argument("--num_images_per_prompt", type=int, default=2)
     parser.add_argument("--guidance_scale", type=float, default=7.5)
     parser.add_argument("--output_dir", type=str, default="inference_results/img2img")
     parser.add_argument("--concept_image", type=str, default="image/2.jpg")
     parser.add_argument("--locon_model", type=str, default="output/locon-augmented-kr-leosam-512-32-3e-4-reg-small-1500")
     parser.add_argument("--strength", type=float, default=0.5)
-    parser.add_argument("--prompt", type=str, default="leogirl, <hoge> person, realistic Documentary photography, detailed face cleavage, realistic, photorealistic, looking at the viewer")
+    parser.add_argument("--target_width", type=int, default=1024)
+    parser.add_argument("--target_height", type=int, default=1024)
+    parser.add_argument("--prompt", type=str, default="leogirl, <hoge> person, detailed face cleavage, realistic, photorealistic")
     parser.add_argument("--negative_prompt", type=str, default= "(worst quality, low quality, cgi, bad eye, worst eye, illustration, cartoon), deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, open mouth")
     parser.add_argument("--algo", type=str, default="locon")
+    parser.add_argument("--mode", type=str, default="i2i")
+    parser.add_argument("--ckpt_path", type=str, default="models/leosam.safetensors")
     return parser.parse_args()
 
 
 # load SDXL pipeline
-checkpoint_path =  "./models/leosam.safetensors"
-pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(
-    checkpoint_path,
-    local_file_only=True,
-    torch_dtype=torch.float16,
-)
 
 if __name__ == "__main__":
     args = parse_args()
+    if args.mode == "i2i":
+        pipe = StableDiffusionXLImg2ImgPipeline.from_single_file(
+            args.ckpt_path,
+            local_file_only=True,
+            torch_dtype=torch.float16,
+        )
+    elif args.mode == "t2i":
+        pipe = StableDiffusionXLPipeline.from_single_file(
+            args.ckpt_path,
+            local_file_only=True,
+            torch_dtype=torch.float16,
+        )
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
@@ -75,25 +85,37 @@ if __name__ == "__main__":
     )
 
     concept_image = Image.open(args.concept_image).convert("RGB")
-    width, height = concept_image.size
-    ratio = height / width 
-    new_width = 1024
-    new_height = int(new_width * ratio)
-
-    concept_image = concept_image.resize((new_width, new_height))
-
-      
-    images = pipe(
-        image=concept_image,
-        prompt=args.prompt,
-        negative_prompt=args.negative_prompt,
-        guidance_scale=args.guidance_scale,
-        # width=args.target_width,
-        # height=args.target_height,
-        num_images_per_prompt=args.num_images_per_prompt,
-        num_inference_steps=args.num_inference_steps,
-        # aesthetic_score=args.ae/sthetic_score,
-        strength=args.strength,
-    ).images
+    # width, height = concept_image.size
+    # ratio = height / width 
+    # new_width = 1024
+    # new_height = int(new_width * ratio)
+    # print("new width: ", new_width)
+    # print("new height: ", new_height)
+    # concept_image = concept_image.resize((new_width, new_height))
+    # concept_image = concept_image.resize((1024, 1024))
+    if args.mode == "i2i":
+        images = pipe(
+            image=concept_image,
+            prompt=args.prompt,
+            negative_prompt=args.negative_prompt,
+            guidance_scale=args.guidance_scale,
+            # width=args.target_width,
+            # height=args.target_height,
+            num_images_per_prompt=args.num_images_per_prompt,
+            num_inference_steps=args.num_inference_steps,
+            # aesthetic_score=args.ae/sthetic_score,
+            strength=args.strength,
+        ).images
+    elif args.mode == "t2i":
+        images = pipe(
+            prompt=args.prompt,
+            negative_prompt=args.negative_prompt,
+            guidance_scale=args.guidance_scale,
+            width=args.target_width,
+            height=args.target_height,
+            num_images_per_prompt=args.num_images_per_prompt,
+            num_inference_steps=args.num_inference_steps,
+        ).images
+        
     for i, img in enumerate(images):
         img.save(f"{args.output_dir}/{i}.png")
