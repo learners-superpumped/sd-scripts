cb12573be3a8888573b799a11dbc6136d4e9baec
root <roundtable@gmail.com>
2023-10-03 07:22:42 +0000

    update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,48 +1,25 @@
 accelerate launch --num_cpu_threads_per_process 1 sdxl_train_network.py \
     --pretrained_model_name_or_path=models/leosam.safetensors \
     --dataset_config=_typos.toml \
-    --output_dir=output/locon-augmented-kr-leosam-512-32-3e-4-reg-2000-2-1\
+    --output_dir=output/locon-leosam-cropped_face_1024_1024-8-4e-4-reg-2000\
     --output_name=test \
     --max_train_steps=2000 \
     --save_model_as=safetensors \
     --prior_loss_weight=1.0 \
-    --learning_rate=3e-4 \
-    --lr_scheduler="cosine" \
-    --optimizer_type="AdamW8bit" \
+    --learning_rate=4e-4 \
+    --lr_scheduler="linear" \
+    --optimizer_type="Adafactor" \
+    --optimizer_args scale_parameter=False relative_step=False warmup_init=False \
     --xformers \
     --mixed_precision="fp16" \
     --cache_latents \
-    --text_encoder_lr=3e-4 \
+    --text_encoder_lr=4e-4 \
     --gradient_checkpointing \
-    --save_every_n_epochs=1 \
-    --sample_every_n_epochs=1 \
+    --save_every_n_steps=500 \
+    --sample_every_n_steps=200 \
+    --sample_sampler=euler_a \
     --sample_prompts="sample_prompt.json"\
     --no_half_vae \
-    --network_dim=32 \
-    --network_args "conv_dim=32" "conv_alpha=1.0" "algo=locon"\
-    --network_module=lycoris.kohya
-
-
-accelerate launch --num_cpu_threads_per_process 1 sdxl_train_network.py \
-    --pretrained_model_name_or_path=models/leosam.safetensors \
-    --dataset_config=_typos.toml \
-    --output_dir=output/locon-augmented-kr-leosam-512-32-3e-4-reg-3000-2-1\
-    --output_name=test \
-    --max_train_steps=3000 \
-    --save_model_as=safetensors \
-    --prior_loss_weight=1.0 \
-    --learning_rate=3e-4 \
-    --lr_scheduler="cosine" \
-    --optimizer_type="AdamW8bit" \
-    --xformers \
-    --mixed_precision="fp16" \
-    --cache_latents \
-    --text_encoder_lr=3e-4 \
-    --gradient_checkpointing \
-    --save_every_n_epochs=1 \
-    --sample_every_n_epochs=1 \
-    --sample_prompts="sample_prompt.json"\
-    --no_half_vae \
-    --network_dim=32 \
-    --network_args "conv_dim=32" "conv_alpha=1.0" "algo=locon"\
+    --network_dim=8 \
+    --network_args "conv_dim=8" "conv_alpha=1.0" "algo=locon"\
     --network_module=lycoris.kohya
