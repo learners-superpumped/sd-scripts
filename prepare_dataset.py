25af3a90dcb40b10f363df349a3aae42d354dec7
root <roundtable@gmail.com>
2023-10-04 12:44:10 +0000

    Update prepare

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -10,11 +10,11 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_dir', type=str, default='dataset/kr')
     parser.add_argument('--output_dir', type=str, default='dataset/cropped_kr')
-    parser.add_argument('--max_crop_size', type=int, default=512)
+    parser.add_argument('--save_size', type=int, default=1024)
     return parser.parse_args()
 
 
-def crop_face(image: Image.Image, crop_height_rate: float = 1.3, crop_width_rate: float = 1.1):
+def crop_face(image: Image.Image, crop_height_rate: float = 1.0, crop_width_rate: float = 1.0):
     mp_face_detection = mp.solutions.face_detection
     face_detection = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
     image = ImageOps.exif_transpose(image)
@@ -67,18 +67,20 @@ def main(args):
         os.makedirs(args.output_dir)
     
     image_paths = glob.glob(os.path.join(args.dataset_dir, '*'))
-
+    print(image_paths)
     for i, image_path in enumerate(image_paths):
         if not image_path.endswith('.png') and not image_path.endswith('.jpg') and not image_path.endswith('.jpeg'):
             continue
         image = Image.open(image_path)
-        try:
-            crop_left, crop_top, crop_right, crop_bottom, face = crop_face(image)
-        except ValueError:
-            continue
-        face = face.resize((args.max_crop_size, args.max_crop_size))
-        face.save(os.path.join(args.output_dir, os.path.basename(image_path)))
-        print(f'{i+1}/{len(image_paths)}: {image_path} cropped')
+        for crop_rate in [1.0, 1.2, 1.5, 2.0]:
+            try:
+                crop_left, crop_top, crop_right, crop_bottom, face = crop_face(image, crop_height_rate=crop_rate, crop_width_rate=crop_rate)
+            except ValueError:
+                continue
+            face = face.resize((args.save_size, args.save_size))
+            filename =  f"cr-{crop_rate}-{os.path.basename(image_path)}"
+            print(f"Saving {filename}...")
+            face.save(os.path.join(args.output_dir, filename))
 
 
 if __name__ == "__main__":
